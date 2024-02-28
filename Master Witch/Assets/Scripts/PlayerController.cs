f5f1457b1304421a9e277dc8705dc8f34a8ad09e
Luis Henrique Costa Silva Araujo <1117946@sga.pucminas.br>
2024-02-28 00:51:39 -0300

    move player

---
+++ Master Witch/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    PlayerInput playerInput;
+    public float speed;
+    public float speedPlayer;
+
+    CharacterController controller;
+
+    bool groundedPlayer;
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        playerInput = new PlayerInput();
+        playerInput.PlayerControl.Enable();
+
+
+        //inputs para buttons 
+        //na funcao precisa colocar InputAction.CallbackContext context
+        //e o context funciona como um ativador
+        //playerInput.PlayerControl.Movement.performed += MovementPlayer;
+    }
+
+    void FixedUpdate()
+    {
+        MovementPlayer();
+    }
+
+    void MovementPlayer(){
+        Vector2 inputVector = playerInput.PlayerControl.Movement.ReadValue<Vector2>();
+
+        Vector3 move = new Vector3(inputVector.x, 0, inputVector.y).normalized;
+        
+        if(inputVector!=new Vector2(0,0)){
+            Quaternion r = Quaternion.LookRotation(move);
+            transform.rotation = Quaternion.Slerp(transform.rotation, r, speed);
+        }
+        controller.Move(move* Time.deltaTime * speedPlayer);
+        
+    }
+}
