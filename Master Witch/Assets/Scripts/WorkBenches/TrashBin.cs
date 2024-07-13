f0c54d7f96fff6541157a61f63924e6fb592fdf4
Luis Henrique Costa Silva Araujo <80539902+LuisHenrique111@users.noreply.github.com>
2024-07-13 19:32:41 -0300

    scripts bench

---
+++ Master Witch/Assets/Scripts/WorkBenches/TrashBin.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashBin : Bench
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
