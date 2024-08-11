79ba09744116ffe15fca0c8a82bf6a141ac57bf0
Luis Henrique Costa Silva Araujo <80539902+LuisHenrique111@users.noreply.github.com>
2024-08-11 20:41:32 -0300

    bench config

--- Master Witch/Assets/Scripts/WorkBenches/Mortar.cs
+++ Master Witch/Assets/Scripts/WorkBenches/Mortar.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
 public class Mortar : Bench
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        isPreparing.OnValueChanged += (a,b) => visualEffect[0].SetBool("isPreparing", isPreparing.Value);
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public override void Pick(Player player)
     {
-        
+        if (endProgress)
+        {
+            var recipeData = new RecipeData(targetRecipe, ingredients);
+            var objectSpawn = Instantiate(recipeData.TargetFood.foodPrefab, new Vector3(player.assetIngredient.transform.position.x, 1.0f, player.assetIngredient.transform.position.z), Quaternion.identity);
+            objectSpawn.GetComponent<NetworkObject>().Spawn();
+            objectSpawn.GetComponent<NetworkObject>().TrySetParent(player.transform);
+            player.GetComponentInChildren<Ingredient>().itensUsed.Add(recipeData);
+            Reset();
+        }
+    }
+    public override void Drop(Player player)
+    {
+        var interact = player.GetComponentInChildren<Ingredient>();
+        endProgress = false;
+        AddIngredient(interact.food);
+        progress();
+        interact.DestroySelf();
+        player.isHand.Value = false;
     }
 }
